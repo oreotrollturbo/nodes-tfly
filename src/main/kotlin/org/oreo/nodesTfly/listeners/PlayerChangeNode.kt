55561cccc4464a2f6528ea5ccaaa843ce71b181b
oreotrollturbo <oreotrollturbo@gmail.com>
2024-09-18 09:33:56 +0200

    Stopped players from leashing entities and fly

--- src/main/kotlin/org/oreo/nodesTfly/listeners/PlayerChangeNode.kt
+++ src/main/kotlin/org/oreo/nodesTfly/listeners/PlayerChangeNode.kt
@@ -1,9 +1,21 @@
 package org.oreo.nodesTfly.listeners
 
+import org.bukkit.entity.ArmorStand
+import org.bukkit.entity.Boat
+import org.bukkit.entity.Camel
+import org.bukkit.entity.Donkey
+import org.bukkit.entity.Horse
+import org.bukkit.entity.LivingEntity
+import org.bukkit.entity.Llama
+import org.bukkit.entity.Mule
+import org.bukkit.entity.Pig
 import org.bukkit.entity.Player
+import org.bukkit.entity.Strider
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerMoveEvent
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
 import org.oreo.nodesTfly.NodesTfly
 import org.oreo.nodesTfly.java.GetNodesInfo
 import org.spigotmc.event.entity.EntityMountEvent
@@ -13,6 +25,8 @@ import java.util.*
 
 
 class PlayerChangeNode(private val plugin : NodesTfly) : Listener {
+
+    private val debuffDuration = plugin.config.getInt("fly-abuse-debuff-duration")
 
     @EventHandler
     fun onPlayerEnterTown(e: PlayerMoveEvent) {
@@ -34,12 +48,19 @@ class PlayerChangeNode(private val plugin : NodesTfly) : Listener {
         val fromX = e.from.blockX
         val fromZ = e.from.blockZ
 
+        val chunkFrom = world.getChunkAt(fromX,fromZ)
+
         val toX = e.to.blockX
         val toZ = e.to.blockZ
 
+        val chunkTo = world.getChunkAt(toX,toZ)
 
-        // check if player chunk changed
-        checkNotNull(world)
+        if (chunkFrom == chunkTo){
+            return
+        }
+
+        unleashEntities(player)
+
         val fromTerritory: Territory? = Nodes.getTerritoryFromBlock(fromX, fromZ)
 
         val toTerritory: Territory? = Nodes.getTerritoryFromBlock(toX, toZ)
@@ -61,5 +82,36 @@ class PlayerChangeNode(private val plugin : NodesTfly) : Listener {
         player.allowFlight = false
     }
 
+    private fun unleashEntities(player: Player){
+
+        for (entity in player.getNearbyEntities(5.0, 5.0, 5.0)) {
+
+            if (entity !is LivingEntity){
+                return
+            }
+
+            if (entity is Horse || entity is Mule || entity is Donkey || entity is Llama || entity is Pig ||
+                entity is ArmorStand || entity is Strider || entity is Camel) {
+
+                if (entity.isLeashed && entity.leashHolder == player) {
+                    entity.setLeashHolder(null)
+
+                    player.allowFlight = false
+
+                    player.addPotionEffect(PotionEffect(PotionEffectType.POISON, debuffDuration * 20,1))
+                    player.addPotionEffect(PotionEffect(PotionEffectType.BLINDNESS, debuffDuration * 20,3))
+                    player.addPotionEffect(PotionEffect(PotionEffectType.WEAKNESS, debuffDuration * 20, 87))
+
+                    sendTitle(player, "§cYou thought bitch","§eNo abusing for u",
+                        10, 65, 20)
+                }
+            }
+        }
+    }
+
+
+    private fun sendTitle(player: Player, title: String?, subtitle: String?, fadeIn: Int, stay: Int, fadeOut: Int) {
+        player.sendTitle(title, subtitle, fadeIn, stay, fadeOut)
+    }
 
 }
